2cbff936c8976ec46bddd3bda0cb393e191b4630
soomin0923 <102664418+soomin0923@users.noreply.github.com>
2022-11-12 21:38:37 +0900

    Update page004.js

--- page004/page004.js
+++ page004/page004.js
@@ -104,7 +104,7 @@ function setup() {
   textAlign(LEFT, TOP);
   textData = getTextData(message);
   dotCordinate = getCordinates();
-  imgs = createParticles(scaleRate, 200, 300);
+  imgs = createParticles(scaleRate, 100, 300);
   
 
 }
